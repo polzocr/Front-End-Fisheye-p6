8ced7c11bf6e7f491c0d9906dfad56ef3e34e9f5
Polzocr <micro.cpt.62@outlook.fr>
2022-10-30 18:59:06 +0100

    ajout de la modal contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,9 @@
 function displayModal() {
+    const body = document.querySelector('body')
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    body.style.overflow = 'hidden';
+
 }
 
 function closeModal() {
