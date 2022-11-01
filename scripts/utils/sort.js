9a55bf3ffd660c88611ed15ad5877d63ee1922af
Polzocr <micro.cpt.62@outlook.fr>
2022-11-01 17:57:43 +0100

    fin du dropdown de tri

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -1,6 +1,8 @@
 let dropdown = document.querySelector(".dropdown")
 dropdown.onclick = function() {
+    addBorder()
     dropdown.classList.toggle("active")
+
 }
 
 const date = document.getElementById('date')
@@ -10,16 +12,19 @@ date.style.display = 'none'
 
 date.addEventListener('click', function(){
     changeText('Date')
+    addBorder()
     const dateMedia = new PhotographerPage('media')
     dateMedia.displayMedia('date')
 })
 famous.addEventListener('click', function(){
     changeText('Popularité')
+    addBorder()
     const famousMedia = new PhotographerPage('media')
     famousMedia.displayMedia('famous')
 })
 title.addEventListener('click', function(){
     changeText('Titre')
+    addBorder()
     const titleMedia = new PhotographerPage('media')
     titleMedia.displayMedia('title')
 })
@@ -42,5 +47,20 @@ title.addEventListener('click', function(){
       famous.style.display = 'block'
     }
   }
+
+function addBorder(){
+    let value = document.getElementById('text').value
+    const dateSpan = document.querySelector('#date span')
+    const famousSpan = document.querySelector('#famous span')
+    if(value == 'Popularité' || value == 'Titre'){
+        dateSpan.style.borderBottom = '2px solid white'
+        dateSpan.style.borderTop = '2px solid white'
+        famousSpan.style.border = 'none'
+    } else {
+        famousSpan.style.borderBottom = '2px solid white'
+        famousSpan.style.borderTop = '2px solid white' 
+        dateSpan.style.border = 'none'
+    }
+}
   
 
